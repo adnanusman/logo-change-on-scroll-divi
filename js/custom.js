a2123e9630f2be28a64eb0468f38817ba6d013c1
Adnan Usman <adnantu@hotmail.com>
2018-03-05 12:13:20 -0800

    Update custom.js
    Changed URLs and added comments

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,10 @@
 window.onscroll = function() {
 	var logo = document.getElementById('logo');
     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-	    logo.setAttribute('src', 'https://www.portofinos.com/wp-content/uploads/logo.png');
+	// Change the URL to the logo you want displayed on scroll
+        logo.setAttribute('src', 'http://www.example.com/wp-content/uploads/logo.png');
     } else {
-        logo.setAttribute('src', 'https://www.portofinos.com/wp-content/uploads/portofinos_LOGO_slanted.png');
+	// Change the URL to the logo that is displayed on page load
+        logo.setAttribute('src', 'http://www.example.com/wp-content/uploads/logo_slanted.png');
     }
 }
